89abf5f308664154880b77d1d0f4d5ca5b2a2603
Chintan2495 <chintanparmar2495@gmail.com>
2026-07-02 19:51:49 +0530

    Add files via upload

---
+++ t1.py
@@ -0,0 +1,158 @@
+# Lecture 1
+
+# print( "Hello Chintan")
+# print("I am from Anand")
+# print("Welcome to Data Analytics")
+
+
+# name=input("your name")
+# age=input("your age")
+# print( "my name is",name)
+# print( "my age is",age)
+
+
+# name=input("enter name:")
+# course=input("enter course:")
+# fees=input("enter fees:")
+# print(name,course,fees)
+
+# student_name= "Chintan"
+# marks= 80
+# passed= True
+# print(student_name)
+# print(marks)
+# print(passed)
+
+
+# a=10
+# b=20
+# c=30
+# a,b=b,a
+# print("a=",a)
+# print("b=",b)
+
+
+# num=10
+# decimal=5.5
+# text="Data Analytics"
+# status= True
+# print(int(num))
+# print(float(decimal))
+# print(str(text))
+# print(bool(status))
+
+# x=10
+# y=5.5
+# result=x+y
+# print(result)
+# print(float(result))
+
+# num1=float(input("enter first number"))
+# num2=float(input("enter second number"))
+# print("addition=", num1 + num2)
+# print("substraction=", num1 - num2)
+# print("multiplicationn=", num1 * num2)
+# print("divison=", num1 / num2)
+# print("modulus=", num1 % num2)
+
+
+
+# a=input("enter true/false:")=="true"
+# b=input("enter true/false:")=="true"
+# print("AND=", a and b)
+# print("OR=", a or b)
+# print("NOT A=", not a)
+
+# num=int(input("enter number"))
+# num +=5
+# print("after +=5",num)
+# num-=2
+# print("after -=2",num)
+# num *=3
+# print("after *=3",num)
+
+
+# text=input("enter string: ")
+# char=input("enter character: ")
+# print(char in text)
+
+
+# a=[1,2,3]
+# b=a
+# c=[1,2,3]
+# print(a is b)
+# print(a is not c)
+
+
+# Lecture 2
+
+# num=int(input("enter the number:"))
+# if num % 2 == 0:
+#     print("even number")
+# else:
+#     print("odd number") 
+
+
+# number=float(input("enter the number:"))
+# if number>0:
+#     print("positive")
+# elif number<0:
+#     print("negative")
+# else:
+#     print("zero")
+
+
+
+
+
+
+# Lecture 3
+
+# For Loop
+# for i in range(101):
+#     print(i)
+
+# for i in range(1,10,2):
+#     print(i)
+
+ for i in range(2,11,2):
+#     print(i)
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
